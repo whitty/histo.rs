7216ebd8f68ba61e9c80438f3f92c3774557dc1c
Greg Whiteley <greg.whiteley@gmail.com>
2023-12-16 22:32:02 +1100

    main: drop example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use asciigraph::Graph;
 use clap::{Parser, ArgAction::Append};
 
 /// Quick and dirty analyzer for file generating histograms from
@@ -39,23 +38,6 @@ fn main() {
             println!("{}", h.draw());
         }
     }
-
-    let mut g1 = Graph::default();
-
-    g1.set_1d_data(vec![0, 1, 1, 0, 2, 0, 1, 2, 0, 0, 0, 1, 0, 1000])
-    //    .set_y_min(0)
-    //    .set_y_max(4)
-    //    .set_plot_height(20)
-        .set_block_width(3)
-    //    .set_y_label_margin(1)
-    //    .set_title(String::from("HEllo 23123123"))
-        .set_paddings([1;4])
-        .set_big_title(true)
-        .set_x_axis_label(String::from("x_axis_label\nxz"))
-        .set_y_axis_label(String::from("y_axis_label\nyy"));
-    let x = g1.draw();
-
-    println!("{}", x);
 }
 
 // Claps' built-in self test
